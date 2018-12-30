827376d4a4d94d37aa004b2358c2ba3d32e25e7b
JerryKwon <kwonyoung234@gmail.com>
2018-12-30 15:41:05 +0900

    Updates

---
+++ precache-manifest.022e5d4c60e1dedfc378e519080e2038.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "46d89eb6011a77090e9e",
+    "url": "/movie_app/static/js/main.46d89eb6.chunk.js"
+  },
+  {
+    "revision": "22fdec2062f68dfa3e16",
+    "url": "/movie_app/static/js/1.22fdec20.chunk.js"
+  },
+  {
+    "revision": "46d89eb6011a77090e9e",
+    "url": "/movie_app/static/css/main.954b7cdf.chunk.css"
+  },
+  {
+    "revision": "f796f4eeaf3d17ac1599d5d69722dce8",
+    "url": "/movie_app/index.html"
+  }
+];
